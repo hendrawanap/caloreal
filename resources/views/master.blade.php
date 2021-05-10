8c509d2a4d05832b2d26748e7fb5b637747814f9
Hendrawan <onyxtata11@gmail.com>
2021-05-10 18:27:13 +0700

    cleaning

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,18 +1,8 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous"> --}}
-    {{-- <style>
-      @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-      body {
-        font-family: 'Poppins', sans-serif; 
-      }
-    </style> --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>@yield('title')</title>
   </head>
@@ -29,9 +19,8 @@
     @yield('container')
     <footer class="bg-primary py-6">
       <div class="container mx-auto text-white text-center text-sm">
-        2021 Made by Daking
+        2021 Made by Daking🦆
       </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
   </body>
 </html>
