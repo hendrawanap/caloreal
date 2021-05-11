a382cbf0e299f58bb13d093cf9a5d8aa49193169
MRifqiFz <mrifqi389@gmail.com>
2021-05-11 23:05:10 +0700

    menambahkan migration foods, menus, bmrs

--- database/migrations/2021_05_11_153758_create_menus_table.php
+++ database/migrations/2021_05_11_153758_create_menus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMenusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('menus_table', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->string('avatar', null);
+            $table->foreignId('food_id');
+            $table->foreignId('user_id');
+            $table->string('menu_name');
+            $table->integer('total_calorie');
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('menus_table');
     }
 }
