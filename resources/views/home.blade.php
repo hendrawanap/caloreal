ff09ea7cc531482646f2dde59323ea212a248f23
ekotyoo <ekeprasetyo@gmail.com>
2021-05-12 00:01:04 +0700

    coba style login (engko ubah en)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
   <div class="container mx-auto pt-20 pb-44">
     <h1 class="text-4xl font-bold mb-2 text-primary">Hitung Indeks Massa Tubuh</h1>
     <h1 class="text-4xl font-bold mb-10 text-primary">Asupan Kalori Harian</h1>
-    <a class="border-2 rounded-xl border-black bg-primary px-9 py-2 text-white text-lg">Hitung Sekarang</a>
+    <a href="#" class="border-2 rounded-xl border-black bg-primary px-9 py-2 text-white text-lg">Hitung Sekarang</a>
   </div>
 </div>
 <div class="container mt-10 mb-24 mx-auto">
