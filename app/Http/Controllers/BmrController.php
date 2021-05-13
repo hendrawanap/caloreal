a65d5f47542c487b52232638b58e39ccd32145b1
ekotyoo <ekeprasetyo@gmail.com>
2021-05-13 22:17:38 +0700

    Menambahkan auth check pada bmr.store

--- app/Http/Controllers/BmrController.php
+++ app/Http/Controllers/BmrController.php
@@ -20,37 +20,38 @@ class BmrController extends Controller
             'height' => 'required|max:3',
             'weight' => 'required|max:3',
         ]);
-        $bmr = Bmr::create([
-            'user_id' => auth()->user()->id,
-            'sex' => $request->sex,
-            'age' => $request->age,
-            'height' => $request->height,
-            'weight' => $request->weight,
-        ]);
+        if (auth()->check()) {
+            $bmr = Bmr::create([
+                'user_id' => auth()->user()->id,
+                'sex' => $request->sex,
+                'age' => $request->age,
+                'height' => $request->height,
+                'weight' => $request->weight,
+            ]);
+        }
 
-        $bmi = array('value' => $request->weight / pow($request->height * 0.01, 2));
 
-        if($bmi['value'] < 17)
-        {
-            $bmi['category'] = 'Sangat Kurus';    
-        } 
-        elseif($bmi['value'] >= 17 && $bmi['value'] < 18.4)
-        {
+        $bmi = $this->calculateBMI($request);
+
+        return view('bmr.index', compact('bmi'));
+    }
+
+    public function calculateBMI($input)
+    {
+        $bmi = array('value' => $input->weight / pow($input->height * 0.01, 2));
+
+        if ($bmi['value'] < 17) {
+            $bmi['category'] = 'Sangat Kurus';
+        } elseif ($bmi['value'] >= 17 && $bmi['value'] < 18.4) {
             $bmi['category'] = 'Kurus Ringan';
-        }
-        elseif($bmi['value'] >= 18.4 && $bmi['value'] < 25)
-        {
+        } elseif ($bmi['value'] >= 18.4 && $bmi['value'] < 25) {
             $bmi['category'] = 'Normal';
-        }
-        elseif($bmi['value'] >= 25 && $bmi['value'] < 27)
-        {
+        } elseif ($bmi['value'] >= 25 && $bmi['value'] < 27) {
             $bmi['category'] = 'Gemuk Ringan';
-        }
-        else
-        {
+        } else {
             $bmi['category'] = 'Sangat Gemuk';
         }
 
-        return view('bmr.index', compact('bmi'));
+        return $bmi;
     }
 }
