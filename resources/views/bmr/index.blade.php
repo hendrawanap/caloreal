2d2863fc90976e9763a42bdcbe300f44d6e91c8d
Hendrawan <onyxtata11@gmail.com>
2021-05-15 19:25:54 +0700

    mengatur tinggi heading

--- resources/views/bmr/index.blade.php
+++ resources/views/bmr/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="bg-ternary relative md:pt-40">
   <img src="{{ asset('img/Image-BMI.png') }}" alt="alt" class="pt-40 pb-10 px-10 md:max-h-72 lg:max-h-full md:absolute md:bottom-0 md:right-0 md:p-0">
-  <div class="container mx-auto h-48 md:h-96 flex flex-col items-center md:block sm:h-72">
+  <div class="container mx-auto h-48 md:h-72 flex flex-col items-center md:block sm:h-72">
     <h1 class="relative z-20 text-2xl font-bold mb-2 text-center md:text-left text-primary tracking-wide sm:text-3xl md:text-4xl lg:text-5xl sm:font-semibold">Indeks Massa Tubuh</h1>
     <p class="relative z-20 text-primary max-w-md xl:max-w-xl text-center md:text-left sm:text-lg">
       Indeks Massa Tubuh (IMT) adalah nilai ukur untuk mengetahui status gizi seseorang berdasarkan berat dan tinggi badannya.
