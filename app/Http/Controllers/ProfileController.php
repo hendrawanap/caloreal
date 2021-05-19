168e4e56c263e9c8483ba6ca800edee998910418
ekotyoo <ekeprasetyo@gmail.com>
2021-05-19 10:38:40 +0700

    Membuat card user pada profile.index dinamis

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bmr;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -11,7 +12,34 @@ class ProfileController extends Controller
     $this->middleware('auth');
   }
 
-  public function index() {
-    return view('profile.index');
+  public function index()
+  {
+    $bmr = Bmr::where('user_id', auth()->user()->id)->first();
+  
+    $bmi = $this->calculateBMI($bmr->weight, $bmr->height);
+
+    return view('profile.index', [
+      'bmr' => $bmr,
+      'bmi' => $bmi
+    ]);
+  }
+
+  public function calculateBMI($weight, $height)
+  {
+    $bmi = array('value' => $weight / pow($height * 0.01, 2));
+
+    if ($bmi['value'] < 17) {
+      $bmi['category'] = 'Sangat Kurus';
+    } elseif ($bmi['value'] >= 17 && $bmi['value'] < 18.4) {
+      $bmi['category'] = 'Kurus Ringan';
+    } elseif ($bmi['value'] >= 18.4 && $bmi['value'] < 25) {
+      $bmi['category'] = 'Normal';
+    } elseif ($bmi['value'] >= 25 && $bmi['value'] < 27) {
+      $bmi['category'] = 'Gemuk Ringan';
+    } else {
+      $bmi['category'] = 'Sangat Gemuk';
+    }
+
+    return $bmi;
   }
 }
