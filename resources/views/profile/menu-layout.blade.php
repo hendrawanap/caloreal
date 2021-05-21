722e2202faf9687af1598872b9834d79ebcd4bb6
ekotyoo <ekeprasetyo@gmail.com>
2021-05-21 11:34:22 +0700

    Modal tambah menu

--- resources/views/profile/menu-layout.blade.php
+++ resources/views/profile/menu-layout.blade.php
@@ -1,9 +1,11 @@
 <div class="flex flex-1 flex-col text-primary">
     <div class="flex">
         <div class="flex flex-1 flex-col max-w-lg mr-12">
-            <div wire:loading.remove class="text-xl font-semibold">List Menu Saya</div>
-            <div wire:loading wire:target="setMenu" class="text-xl font-semibold">
-                Loading...
+            <div class="flex justify-between">
+                <div class="text-xl font-semibold">List Menu Saya</div>
+                <div wire:loading wire:target="setMenu" class="text-xl font-semibold">
+                    Loading...
+                </div>
             </div>
             <div class="relative px-5 mt-2">
                 <button class="absolute z-10 left-0 top-16 material-icons p-2 bg-secondary rounded-full "
@@ -11,7 +13,7 @@
                 <div class="flex overflow-x-auto scrollbar-hide" id="my-menu">
                     @foreach ($userMenus as $userMenu)
                         <div wire:click="setMenu({{ $userMenu }}, {{ true }})"
-                            class="flex-shrink-0 w-48 h-36 hover:bg-gray-200 mr-2 rounded-xl p-3 cursor-pointer {{ $selected == $userMenu->name ? 'bg-gray-200' : 'bg-gray-50'}}">
+                            class="flex-shrink-0 w-48 h-36 hover:bg-gray-200 mr-2 rounded-xl p-3 cursor-pointer {{ $selected == $userMenu->name ? 'bg-gray-200' : 'bg-gray-50' }}">
                             <div class="font-semibold text-lg">{{ $userMenu->name }}</div>
                             @foreach ($userMenu->foods->take(4) as $food)
                                 <div class="ml-3 mt-1 text-sm">{{ $food->foodname }}</div>
@@ -33,21 +35,24 @@
                 <button class="absolute left-0 top-16 material-icons p-2 bg-secondary rounded-full"
                     id="rec-menu-prev">arrow_left</button>
                 <div class="flex overflow-x-auto" id="rec-menu">
-                  @foreach ($recMenus as $recMenu)
-                  <div wire:click="setMenu({{ $recMenu }}, {{ 0 }})"
-                      class="flex-shrink-0 w-48 h-36 hover:bg-gray-200 mr-2 rounded-xl p-3 cursor-pointer {{ $selected == $recMenu->name ? 'bg-gray-200' : 'bg-gray-50'}}">
-                      <div class="font-semibold text-lg">{{ $recMenu->name }}</div>
-                      @foreach ($recMenu->foods->take(4) as $food)
-                          <div class="ml-3 mt-1 text-sm">{{ $food->foodname }}</div>
-                      @endforeach
-                  </div>
-                  @endforeach
+                    @foreach ($recMenus as $recMenu)
+                        <div wire:click="setMenu({{ $recMenu }}, {{ 0 }})"
+                            class="flex-shrink-0 w-48 h-36 hover:bg-gray-200 mr-2 rounded-xl p-3 cursor-pointer {{ $selected == $recMenu->name ? 'bg-gray-200' : 'bg-gray-50' }}">
+                            <div class="font-semibold text-lg">{{ $recMenu->name }}</div>
+                            @foreach ($recMenu->foods->take(4) as $food)
+                                <div class="ml-3 mt-1 text-sm">{{ $food->foodname }}</div>
+                            @endforeach
+                        </div>
+                    @endforeach
                 </div>
                 <button class="absolute right-0 top-16 material-icons p-2 bg-secondary rounded-full"
                     id="rec-menu-next">arrow_right</button>
             </div>
         </div>
     </div>
-    
+
     @livewire('menu-details', ['menu' => $userMenus[0], 'isUserMenu' => true])
+    @if ($showForm)
+        @livewire('menu-form')
+    @endif
 </div>
