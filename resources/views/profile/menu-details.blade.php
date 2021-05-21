03888046ef9f228471b1bf86836683ec385a5d42
Hendrawan <onyxtata11@gmail.com>
2021-05-21 20:59:05 +0700

    fix menu-details

--- resources/views/profile/menu-details.blade.php
+++ resources/views/profile/menu-details.blade.php
@@ -12,17 +12,17 @@
     </div>
     <div class="bg-gray-50 rounded-xl px-4 py-5 w-full mt-3">
         <div class="flex justify-between gap-x-4 text-sm text-center font-semibold">
-            <button wire:click="updateFoodsTable('sarapan')"
-                class="flex-1 border py-3 rounded-xl {{ $selected === 'sarapan' ? 'border-secondary bg-secondary' : 'border-primary' }}"
+            <button wire:click="updateFoodsTable('Sarapan')"
+                class="flex-1 border py-3 rounded-xl {{ $selected === 'Sarapan' ? 'border-secondary bg-secondary' : 'border-primary' }}"
                 id="btn-sarapan">Sarapan</button>
-            <button wire:click="updateFoodsTable('makanSiang')"
-                class="flex-1 border py-3 rounded-xl {{ $selected === 'makanSiang' ? 'border-secondary bg-secondary' : 'border-primary' }}"
+            <button wire:click="updateFoodsTable('Makan Siang')"
+                class="flex-1 border py-3 rounded-xl {{ $selected === 'Makan Siang' ? 'border-secondary bg-secondary' : 'border-primary' }}"
                 id="btn-makan-siang">Makan Siang</button>
-            <button wire:click="updateFoodsTable('makanMalam')"
-                class="flex-1 border border-primary py-3 rounded-xl {{ $selected === 'makanMalam' ? 'border-secondary bg-secondary' : 'border-primary' }}"
+            <button wire:click="updateFoodsTable('Makan Malam')"
+                class="flex-1 border border-primary py-3 rounded-xl {{ $selected === 'Makan Malam' ? 'border-secondary bg-secondary' : 'border-primary' }}"
                 id="btn-makan-malam">Makan Malam</button>
-            <button wire:click="updateFoodsTable('snack')"
-                class="flex-1 border border-primary py-3 rounded-xl {{ $selected === 'snack' ? 'border-secondary bg-secondary' : 'border-primary' }}"
+            <button wire:click="updateFoodsTable('Snack')"
+                class="flex-1 border border-primary py-3 rounded-xl {{ $selected === 'Snack' ? 'border-secondary bg-secondary' : 'border-primary' }}"
                 id="btn-snack">Snack</button>
         </div>
         @livewire('foods-table', ['foods' => $menu->sarapan])
