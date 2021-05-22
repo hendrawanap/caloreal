811aa2d5749b97ba622736b5ce8d592123b99e57
Hendrawan <onyxtata11@gmail.com>
2021-05-22 10:52:40 +0700

    style tombol edit dan hapus makanan

--- resources/views/profile/foods-table.blade.php
+++ resources/views/profile/foods-table.blade.php
@@ -2,10 +2,10 @@
     <table class="table-fixed w-full text-left mt-5">
         <thead class="border-b border-gray-400">
             <tr>
-                <th class="w-1/2 text-lg font-semibold">Makanan</th>
-                <th class="w-1/4 text-lg font-semibold">Porsi</th>
-                <th class="w-1/4 text-lg font-semibold">Kalori</th>
-                <th></th>
+                <th class="w-6/12 text-lg font-semibold">Makanan</th>
+                <th class="w-3/12 text-lg font-semibold">Porsi</th>
+                <th class="w-2/12 text-lg font-semibold">Kalori</th>
+                <th class="w-1/12"></th>
             </tr>
         </thead>
         <tbody class="border-b border-gray-400 pb-2">
@@ -14,7 +14,10 @@
                     <td class="py-2">{{ $food['foodname'] }}</td>
                     <td class="py-2">{{ $food['quantity'] }} gram</td>
                     <td class="py-2">{{ $food['calorie'] }} kkal</td>
-                    <td wire:click="detachFood({{ $food['id'] }})">Hapus</td>
+                    <td class="material-icons py-2" >
+                    <span class="cursor-default">edit</span>
+                    <span class="cursor-default" wire:click="detachFood({{ $food['id'] }})">delete</span>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
